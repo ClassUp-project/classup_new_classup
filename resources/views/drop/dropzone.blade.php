0480b57cfc38801c654bff571d63d5a51520b1ae
Your Name <you@example.com>
2021-04-17 15:31:20 +0200

    dropzone operationnelle

--- resources/views/drop/dropzone.blade.php
+++ resources/views/drop/dropzone.blade.php
@@ -11,11 +11,6 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
-
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
 
         <link rel="stylesheet" href="{{mix('/css/app.css')}}">
 
@@ -27,26 +22,8 @@
         <div class="flex-center position-ref full-height" id="app">
 
 
-            <div class="container mt-2">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="mt-2 mb-2">Drag & Drop File Uploading using Laravel 8 Dropzone JS</h1>
+            <example-component></example-component>
 
-                        <form action="{{ route('download',[Auth::user()]) }}" method="post" enctype="multipart/form-data" id="image-upload" class="dropzone">
-                            @csrf
-                            <div>
-                                <h3>Upload Multiple Image By Click On Box</h3>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <script type="text/javascript">
-                Dropzone.options.imageUpload = {
-                    maxFilesize         :       1,
-                    acceptedFiles: ".jpeg,.jpg,.png,.gif,.pdf"
-                };
-            </script>
 
             @if(session()->has('message'))
             <div class="alert alert-success">
@@ -54,14 +31,14 @@
             </div>
             @endif
 
-            <div class="container">
+            <div class="container mt-20">
                 <div class="row">
 
                     @foreach ($idFile as $item)
 
 
                     <div class="flex items-stretch bg-gray-200 h-24">
-                        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+                        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 ">
                         <a href="{{ Route('download', $item->iddropzone)}}" download="{{ $item->original }}">
                             {{ Str::limit( $item->thumbnail, 35 )}}
                         </a>
@@ -91,7 +68,7 @@
         </div>
 
         <script src="{{mix('/js/app.js')}}">
-</script>
+        </script>
 
 
 
