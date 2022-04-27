e182ef2df3876e00e10e383e096ed446481b2f00
Stéphane <stephripa@gmail.com>
2022-04-27 17:43:18 +0200

    màj

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -46,7 +46,7 @@
 
                     <div class="grid grid-cols-3 divide-x divide-gray-400 mt-10 ml-48 p-8 ml-4 button-tag-dashboard-div-align">
 
-                        <a id="navigation-to-dashboard" href="/questionnaire/create" class="bg-green-500 hover:bg-green-700 text-white ml-8 py-2 px-4 rounded inline-flex items-center button-tag-dashboard">
+                        <a id="navigation-to-dashboard" href="/questionnaires/create" class="bg-green-500 hover:bg-green-700 text-white ml-8 py-2 px-4 rounded inline-flex items-center button-tag-dashboard">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                             </svg>
