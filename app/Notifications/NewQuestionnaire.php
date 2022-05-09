d002483c6b240caa96d83990d6933524f4608d27
steph-GoodLife <stephripa@gmail.com>
2022-05-09 13:53:37 +0200

    PWA

--- app/Notifications/NewQuestionnaire.php
+++ app/Notifications/NewQuestionnaire.php
@@ -6,23 +6,20 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-use App\Models\Utilisateur;
 
 class NewQuestionnaire extends Notification
 {
     use Queueable;
     private $newQuestionnaire;
-    private $user;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct($newQuestionnaire, $user)
+    public function __construct($newQuestionnaire)
     {
         $this->newQuestionnaire = $newQuestionnaire;
-        $this->user = $user;
     }
 
     /**
@@ -42,11 +39,11 @@ class NewQuestionnaire extends Notification
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail($notifiable, Utilisateur $user)
+    public function toMail($notifiable)
     {
         return (new MailMessage)
                     ->subject('Nouveau questionnaire disponible !')
-                    ->greeting('Salut' , $this->$user['prenom'])
+                    ->greeting('Salut' , $this->newQuestionnaire['prenom'])
                     ->line('Un nouveau quiz vient d\'être publié. Consulte le !')
                     ->line($this->newQuestionnaire['body'])
                     ->action($this->newQuestionnaire['actionText'], $this->newQuestionnaire['Url']);
---
+++ config/laravelpwa.php
@@ -0,0 +1,78 @@
+<?php
+
+return [
+    'name' => 'LaravelPWA',
+    'manifest' => [
+        'name' => env('APP_NAME', 'ClassUp'),
+        'short_name' => 'PWA',
+        'start_url' => '/',
+        'background_color' => '#ffffff',
+        'theme_color' => '#000000',
+        'display' => 'standalone',
+        'orientation'=> 'any',
+        'status_bar'=> 'black',
+        'icons' => [
+            '72x72' => [
+                'path' => '/images/icons/classuplogo.PNG',
+                'purpose' => 'any'
+            ],
+            '96x96' => [
+                'path' => '/img/icons/classuplogo.PNG',
+                'purpose' => 'any'
+            ],
+            '128x128' => [
+                'path' => '/images/icons/classuplogo.PNG',
+                'purpose' => 'any'
+            ],
+            '144x144' => [
+                'path' => '/images/icons/classuplogo.PNG',
+                'purpose' => 'any'
+            ],
+            '152x152' => [
+                'path' => '/images/icons/classuplogo.PNG',
+                'purpose' => 'any'
+            ],
+            '192x192' => [
+                'path' => '/images/icons/classuplogo.PNG',
+                'purpose' => 'any'
+            ],
+            '384x384' => [
+                'path' => '/images/icons/classuplogo.PNG',
+                'purpose' => 'any'
+            ],
+            '512x512' => [
+                'path' => '/images/icons/classuplogo.PNG',
+                'purpose' => 'any'
+            ],
+        ],
+        'splash' => [
+            '640x1136' => '/images/icons/classuplogo.PNG',
+            '750x1334' => '/images/icons/classuplogo.PNG',
+            '828x1792' => '/images/icons/classuplogo.PNG',
+            '1125x2436' => '/images/icons/classuplogo.PNG',
+            '1242x2208' => '/images/icons/classuplogo.PNG',
+            '1242x2688' => '/images/icons/classuplogo.PNG',
+            '1536x2048' => '/images/icons/classuplogo.PNG',
+            '1668x2224' => '/images/icons/classuplogo.PNG',
+            '1668x2388' => '/images/icons/classuplogo.PNG',
+            '2048x2732' => '/images/icons/classuplogo.PNG',
+        ],
+        'shortcuts' => [
+            [
+                'name' => 'Shortcut Link 1',
+                'description' => 'Shortcut Link 1 Description',
+                'url' => '/shortcutlink1',
+                'icons' => [
+                    "src" => "/images/icons/classuplogo.PNG",
+                    "purpose" => "any"
+                ]
+            ],
+            [
+                'name' => 'Shortcut Link 2',
+                'description' => 'Shortcut Link 2 Description',
+                'url' => '/shortcutlink2'
+            ]
+        ],
+        'custom' => []
+    ]
+];
