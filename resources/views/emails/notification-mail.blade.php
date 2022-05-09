ed2eafd9bdfd5a28d14b61618c07ff51f8055f24
steph-GoodLife <stephripa@gmail.com>
2022-05-09 12:12:36 +0200

    ouverture mail

--- resources/views/emails/notification-mail.blade.php
+++ resources/views/emails/notification-mail.blade.php
@@ -1,19 +1,87 @@
-@component('mail::message')
- <!--Title-->
-		<h1>{{ $post['title'] }}</h1>
-        <!--Container-->
-        <h2>
-            Merci pour ton inscription {{$user->prenom}}!
-        </h2>
-        <h3>
-            Class'Up t'aide à réviser facilement tes cours et tes matières en te permattant de répondre à de cours questionniares, regarder des vidéos sur le sujet et lire des documents.<br>
-            Tu peux aussi tester tes ami.e.s en postant tes propres quiz!
-        </h3>
-        <h3>
-            Poste et réponds dès maintenant aux questionnaires dans la matière de ton choix.
-        </h3>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="color-scheme" content="light">
+    <meta name="supported-color-schemes" content="light">
+        <style>
+        @media  only screen and (max-width: 600px) {
+        .inner-body {
+        width: 100% !important;
+        }
 
-        @component('mail::button', ['url' => $post])
-        Tous les cours
-        @endcomponent
-@endcomponent
+        .footer {
+        width: 100% !important;
+        }
+        }
+
+        @media  only screen and (max-width: 500px) {
+        .button {
+        width: 100% !important;
+        }
+        }
+        </style>
+</head>
+<body style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; color: #74787e; height: 100%; hyphens: auto; line-height: 1.4; margin: 0; -moz-hyphens: auto; -ms-word-break: break-all; width: 100% !important; -webkit-hyphens: auto; -webkit-text-size-adjust: none; word-break: break-word;">
+
+    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;"><tr>
+        <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+            <table class="content" width="100%" cellpadding="0" cellspacing="0" role="presentation" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+            <tr>
+            <td class="header" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 25px 0; text-align: center;">
+            <a href="https://markdownmail.com" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #bbbfc3; font-size: 19px; font-weight: bold; text-decoration: none; text-shadow: 0 1px 0 #ffffff; display: inline-block;">
+            <img src="img/classuplogo.png" class="logo" alt="" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; max-width: 100%; border: none;"></a>
+        </td>
+    </tr>
+    <!-- Email Body --><tr>
+        <td class="body" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; border-bottom: 1px solid #edeff2; border-top: 1px solid #edeff2; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+        <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
+    <!-- Body content --><tr>
+    <td class="content-cell" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
+    <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">
+        Merci pour ton inscription {{$user->prenom}}!
+    </h1>
+    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787e; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+        Class'Up t'aide à réviser facilement tes cours et tes matières en te permattant de répondre à de cours questionniares, regarder des vidéos sur le sujet et lire des documents.
+    </p>
+    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787e; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+        Tu peux aussi tester tes ami.e.s en postant tes propres quiz!
+    </p>
+    <h2 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 16px; font-weight: bold; margin-top: 0; text-align: left;">
+        Poste et réponds dès maintenant aux questionnaires dans la matière de ton choix.
+    </h2>
+    <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 30px auto; padding: 0; text-align: center; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;"><tr>
+    <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;"><tr>
+    <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+    <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;"><tr>
+    <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+    <a href="#" class="button button-blue" target="_blank" rel="noopener" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #ffffff; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #3097d1; border-top: 10px solid #3097d1; border-right: 18px solid #3097d1; border-bottom: 10px solid #3097d1; border-left: 18px solid #3097d1;">Blue button</a>
+    </td>
+    </tr></table>
+    </td>
+    </tr></table>
+    </td>
+    </tr></table>
+
+    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787e; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
+    Thanks,<br>
+    Laravel
+    </p>
+
+    <tr>
+    <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+    <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;"><tr>
+    <td class="content-cell" align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
+    <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #aeaeae; font-size: 12px; text-align: center;">© 2022 Class'Up. Tous droits réservés.</p>
+
+    </td>
+    </tr></table>
+    </td>
+    </tr>
+    </table>
+    </td>
+    </tr></table>
+</body>
+</html>
