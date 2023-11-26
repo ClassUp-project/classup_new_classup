2ceb564e39b3cb59db462ad630f3ed34ee03189a
Stéphane <stephripa@gmail.com>
2023-11-26 21:33:25 +0100

    add 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,9 @@
-@extends('errors::minimal')
+@extends('errors::illustrated-layout')
 
 @section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('code', '403 😭')
+@section('message', __('Ben alors... on s\'est perdu dans les couloir ?'))
+
+<div style="background-image: url('/img/coridor.png');"
+    class="absolute pin bg-no-repeat md:bg-right bg-right">
+</div>
--- resources/views/errors/illustrated-layout.blade.php
+++ resources/views/errors/illustrated-layout.blade.php
@@ -466,12 +466,12 @@
 
                     <div class="w-16 h-1 bg-purple-light my-3 md:my-6"></div>
 
-                    <p class="text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">
+                    <p class="relative text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">
                         @yield('message')
                     </p>
 
                     <a href="{{ app('router')->has('home') ? route('home') : url('/') }}">
-                        <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
+                        <button class="relative bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
                             {{ __('Go Home') }}
                         </button>
                     </a>
